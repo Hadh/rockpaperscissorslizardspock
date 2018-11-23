72c81e7fed524a6d74238f6bc8b3dc6da791912e
Hadh <soccerstar.laouini@gmail.com>
2018-11-23 17:03:16 +0100

    added few highlights

--- app.js
+++ app.js
@@ -10,16 +10,39 @@ var scissors_div = document.getElementById("s");
 var lizard_div = document.getElementById("l");
 var spock_div = document.getElementById("sp");
 
+var user_badge = document.getElementById("user-label");
+var computer_badge = document.getElementById("computer-label");
+
 function getComputerChoice() {
     var choices = ["r", "p", "s", "l", "sp"];
     const randomNumber = Math.floor(Math.random() * 5);
     return choices[randomNumber];
 }
 
+function highlight(obj) {
+    var orig = obj.style.background;
+    obj.style.background = '#3ac569';
+    setTimeout(function () {
+        obj.style.background = orig;
+    }, 1000);
+}
+
+function highlightDraw() {
+    var orig1 = user_badge.style.background;
+    var orig2 = computer_badge.style.background;
+    user_badge.style.background = '#dadbdb';
+    computer_badge.style.background = '#dadbdb';
+    setTimeout(function () {
+        user_badge.style.background = orig1;
+        computer_badge.style.background = orig2
+    }, 1000);
+}
+
 function win(userChoice, computerChoice) {
     userScore++;
     const smallUser = "user".fontsize(3).sup();
     const smallComp = "computer".fontsize(3).sup();
+    highlight(user_badge);
     userScore_span.innerHTML = userScore;
     result_div.innerHTML = convert(userChoice) + smallUser + " Beats " + convert(computerChoice) + smallComp + ". You win!";
 }
@@ -28,12 +51,14 @@ function lose(userChoice, computerChoice) {
     computerScore++;
     const smallUser = "user".fontsize(3).sup();
     const smallComp = "computer".fontsize(3).sup();
+    highlight(computer_badge);
     computerScore_span.innerHTML = computerScore;
     result_div.innerHTML = convert(computerChoice) + smallUser + " Beats " + convert(userChoice) + smallComp + ". You Lose!";
 }
 
 function draw() {
     result_div.innerHTML = "It's a Tie!";
+    highlightDraw();
 }
 
 function convert(letter) {
